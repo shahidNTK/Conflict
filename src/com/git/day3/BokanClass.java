ad50907bed9033ea0485dc8ccbaece1e52b33355
bokan <boko1@DESKTOP-KJE9GV6.myfiosgateway.com>
2023-01-14 10:49:39 -0500

    commit

--- src/com/git/day3/BokanClass.java
+++ src/com/git/day3/BokanClass.java
@@ -7,6 +7,8 @@ public class BokanClass {
 	public static void main(String[] args) {
 System.out.println("Java pro");
 System.out.println("Git is fun ...");
+System.out.println("Git is fun ...");
+System.out.println("Git is fun ...");
 	}
 
 }

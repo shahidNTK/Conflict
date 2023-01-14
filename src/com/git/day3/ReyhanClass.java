59de443377a64c889c46b9a2dc18f3e46ef41791
Nick.Tyler <nicktylernt@me.com>
2023-01-14 12:39:59 -0500

    i did a clean up of your code!

--- src/com/git/day3/ReyhanClass.java
+++ src/com/git/day3/ReyhanClass.java
@@ -1,27 +1,26 @@
 package com.git.day3;
 
 public class ReyhanClass {
-public static void main (String args []) {
-		
-		String colorOption= "blue";
+	public static void main(String args[]) {
+
+		String colorOption = "blue";
 		switch (colorOption) {
 		case "Red":
-			System.out.println( "Red Color");
+			System.out.println("Red Color");
 			break;
 		case "Orange":
-			System.out.println ("Orange Color");
+			System.out.println("Orange Color");
 			break;
 		case "Black":
-			System.out.println ("Black Color");
+			System.out.println("Black Color");
 			break;
 		case "blue":
-			System.out.println ("blue Color");
+			System.out.println("blue Color");
 			break;
 		default:
-			System.out.println ("Wrong color,not available");
-				
-			
+			System.out.println("Wrong color,not available");
+
 		}
-}
+	}
 
 }

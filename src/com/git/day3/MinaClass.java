86c900a6ba9c3fb58aa8ec6cb05372a10ca22338
minaalshammari <minaalshammari@172.20.6.1>
2023-01-14 19:37:51 +0300

    commit added

---
+++ src/com/git/day3/MinaClass.java
@@ -0,0 +1,12 @@
+package com.git.day3;
+
+public class MinaClass {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello Git Hub");
+		System.out.println("I Love Git Hub");
+	}
+
+}

9c0bf52b79294da2f73847d9e00a4ba7bd009d1b
Nick.Tyler <nicktylernt@me.com>
2023-01-14 16:41:05 -0500

    wth are you doing lol

--- src/com/git/day3/ReyhanClass.java
+++ src/com/git/day3/ReyhanClass.java
@@ -1,26 +1,25 @@
 package com.git.day3;
 
 public class ReyhanClass {
-public static void main (String args []) {
-		
-		String colorOption= "Orange";
+	public static void main(String args[]) {
+
+		String colorOption = "Orange";
 		switch (colorOption) {
 		case "Red":
-			System.out.println( "Red Color");
+			System.out.println("Red Color");
 			break;
 		case "Orange":
-			System.out.println ("Orange Color");
+			System.out.println("Orange Color");
 			break;
 		case "Black":
-			System.out.println ("Black Color");
+			System.out.println("Black Color");
 			break;
 		default:
-			System.out.println ("Wrong color,not available");
-			
-				
-				
-	Syste.out.println("*** Sorry Mrs:Ryhana i was get busy with my Team, i was not able to get back to you, Hope this changes can make you happy**);			
+			System.out.println("Wrong color,not available");
+
+			System.out.println(
+					"*** Sorry Mrs:Ryhana i was get busy with my Team, i was not able to get back to you, Hope this changes can make you happy");
 		}
-}
+	}
 
 }

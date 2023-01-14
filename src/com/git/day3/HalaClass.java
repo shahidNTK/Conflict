03249a6bd53ec7d907d8e8e65be15df1eb9bc62e
halaalazzawi <halaalazzawi@halas-air-2>
2023-01-14 10:50:06 -0500

    Adding toUpper method

---
+++ src/com/git/day3/HalaClass.java
@@ -0,0 +1,12 @@
+package com.git.day3;
+
+public class HalaClass {
+	
+	public static String toUpper(String str) {
+		String result = "";
+		
+		result = str.toUpperCase();		
+		return result;
+	}
+
+}

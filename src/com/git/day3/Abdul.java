1cb07988a712c09f666a0949cb63af1bd0941e53
Abdul Qadir <Abdul Qadir@YOGA-C930-ABQ.hsd1.tn.comcast.net>
2023-01-14 12:01:59 -0500

    Merge branch 'master' of https://github.com/shahidNTK/Conflict.git

--- src/com/git/day3/Abdul.java
+++ src/com/git/day3/Abdul.java
@@ -3,11 +3,10 @@ package com.git.day3;
 public class Abdul {
 	public static void main(String[] args) {
 		
-		int a =25;
-		int b =30;
+	
 		
-		for (int i = 0; i < args.length; i++) {
-			System.out.println();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("I love Git hub!!! ");
 		}
 		
 	}

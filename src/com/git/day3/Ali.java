67482187ec31abbd73298d415b37d8bd82a620b0
Owner <Owner@DESKTOP-23C0TJ9.fios-router.home>
2023-01-14 12:37:55 -0500

    new done

--- src/com/git/day3/Ali.java
+++ src/com/git/day3/Ali.java
@@ -5,6 +5,7 @@ public class Ali {
 	public static void main(String[] args) {
 		System.out.println("I shoud be able to doit ");
 		System.out.println("I think is done ");
+		System.out.println("why this happen");
 	}
 
 }

02a24aeb025a5a0d9d05335da8842e0bf339ccc0
dost <dost@DESKTOP-5E93V6M>
2023-01-14 12:55:55 -0500

    dsfsfsf

--- src/com/git/day3/FarhadsClass.java
+++ src/com/git/day3/FarhadsClass.java
@@ -2,48 +2,50 @@ package com.git.day3;
 
 public class FarhadsClass {
 
+	public static void main(String[] args) {
 
+		getWhileLoopResult(1, 50);
 
-		public static void main(String[] args) {
+	}
 
-			getWhileLoopResult(1, 50);
+	public static void getWhileLoopResult(int minNum, int maxNum) {
 
-		}
+		int i = 1;
+		while (i <= 50) {
 
-		public static void getWhileLoopResult(int minNum, int maxNum) {
+			System.out.println(i);
 
-			int i = 1;
-			while (i <= 50) {
+			i++;
 
-				System.out.println(i);
-
-				i++;
-
-				System.out.println("I'm not sure what's going on here");
-				
-			}
-
-		}
-		
-		
-		public static void add() {
-			
-			int num = 10-20;
 			System.out.println("I'm not sure what's going on here");
-			System.out.println("I'm not sure what's going on here");
+
 		}
 
 	}
-	
+
+	public static void add() {
+			
+			int num = 10-20;
+			
+		}
+
+}
+
 /**
- * Farhad. I JUST wanted to see if i can bring changes in your class. Sorry. Reyhana
+ * Farhad. I JUST wanted to see if i can bring changes in your class. Sorry.
+ * Reyhana
  */
-	// when for the same block of code when you have two versions
-	// 1. is reomote version
-	// 2. is local version
+// when for the same block of code when you have two versions
+// 1. is reomote version
+// 2. is local version
 
-	
-	
-	
+// Reyhana do you know we have two type of repository Local and Remote, so the code which you see in below that's the remote one, and the above is from my local,
+//so now i can make any change's it will showing both. 	
 
-// } DONT MAKE ROOKIE MISTAKES
+/**
+ * Let's practice our Github tonight in our group study 
+ */
+
+
+
+

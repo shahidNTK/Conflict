80715fe0310d7d88bc7ff48d218789e7bde41110
Nick.Tyler <nicktylernt@me.com>
2023-01-14 10:56:33 -0500

    nick's class

---
+++ src/com/git/day3/NickClass.java
@@ -0,0 +1,11 @@
+package com.git.day3;
+
+public class NickClass {
+	public static void main(String[] args) {
+		
+		for(int i = 0;i<10;i++) {
+			System.out.println("Let's have fun");
+		}
+	}
+
+}

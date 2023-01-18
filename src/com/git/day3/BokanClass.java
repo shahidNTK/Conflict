4df217ff4e044ceb02a78d720b4fb584334d80e1
hara-hora <89297562+hara-hora@users.noreply.github.com>
2023-01-18 03:35:39 -0500

    Update BokanClass.java

--- src/com/git/day3/BokanClass.java
+++ src/com/git/day3/BokanClass.java
@@ -34,12 +34,6 @@ System.out.println("Git is fun ...");
 System.out.println("Git is fun ...");
 System.out.println("Git is fun ...");
 System.out.println("Git is fun ...");
-System.out.println("Git is fun1 ...");
-				System.out.println("Git is fun ...");
-
-		
-		
-		System.out.println("Git is fun ...");
 
 	}
 
